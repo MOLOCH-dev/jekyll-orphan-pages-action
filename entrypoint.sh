970e6d8537b57ef883ced4b46f3f4917c819f9d9
MOLOCH-dev <sabnisanushree@gmail.com>
2021-09-02 18:16:05 +0530

    Fixed EOL

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -l
-
-export LOG=log.txt
-wget --mirror --spider "$1" 2>&1 | tee "$LOG"
-arr=(`grep -P -o -e '(?<=^--....-..-.. ..:..:..--  )(.*)' "$LOG"`)
+export LOG=log.txt;
+wget --mirror --spider "$1" 2>&1 | tee "$LOG";
+arr=(`grep -P -o -e '(?<=^--....-..-.. ..:..:..--  )(.*)' "$LOG"`);
 echo "::set-output name=crawled-urls::$arr"
