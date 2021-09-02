b393701578ab50e69d9086f6ab69c101b872df25
MOLOCH-dev <sabnisanushree@gmail.com>
2021-09-02 18:22:31 +0530

    fixed EOL

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -l
 export LOG=log.txt;
+echo "got after export";
 wget --mirror --spider "$1" 2>&1 | tee "$LOG";
+echo "got after log";
 arr=(`grep -P -o -e '(?<=^--....-..-.. ..:..:..--  )(.*)' "$LOG"`);
 echo "::set-output name=crawled-urls::$arr
