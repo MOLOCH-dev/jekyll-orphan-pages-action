943aedf3444c7f35b28a1aa7a40d4493aff28462
MOLOCH-dev <sabnisanushree@gmail.com>
2021-09-02 18:12:32 +0530

    Changed sh to bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
-#!/bin/sh -l
-declare -a arr
+#!/bin/bash -l
+
 export LOG=log.txt
-wget --no-directories --mirror --spider "$1" 2>&1 | tee "$LOG"
+wget --mirror --spider "$1" 2>&1 | tee "$LOG"
 arr=(`grep -P -o -e '(?<=^--....-..-.. ..:..:..--  )(.*)' "$LOG"`)
 echo "::set-output name=crawled-urls::$arr"
